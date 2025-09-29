5373ec99ffe89d1b19b2d682d6335bf85bb5fb0d
gandol2 <sskim2333@gmail.com>
2025-09-29 20:19:37 +0900

    Agent 역활을 하드코딩

--- 02-NEWS-READER-AGENT/main.py
+++ 02-NEWS-READER-AGENT/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from 02-news-reader-agent!")
+import dotenv
 
+dotenv.load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# https://docs.crewai.com/ko/introduction
+from crewai import Agent, Crew, Task
+from crewai.project import CrewBase, agent, task
+
+@CrewBase
+class 번역Crew:
+    # https://docs.crewai.com/ko/concepts/agents
+    @agent
+    def 번역_agent(self):
+        return Agent(
+            goal="사람들이 헷갈리지 않게 잘 번역하는 번역가가 되는 것",
+            role="한글을 영어로 번역하는 번역가가 되는거야"
+            backstory="너는 미국에서 태어나서 자란 한국인이야. 두 언어 모두 잘하고 문화적 차이도 잘 이해를 하고있지.",
+        )
