55cdef1e4e5f3a2a1d1b63fb8016d3f01898e949
gandol2 <sskim2333@gmail.com>
2025-09-30 00:33:13 +0900

    simple Flow

--- 03-content-pipline-agent/main.py
+++ 03-content-pipline-agent/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from 03-content-pipline-agent!")
+from crewai.flow.flow import Flow, listen, start, router, and_, or_
 
 
-if __name__ == "__main__":
-    main()
+
+class MyFirstFlow(Flow):
+
+    @start()
+    def first(self):
+        print("Hello")
+
+    
+    @listen(first)
+    def second(self):
+        print("World")
+
+    @listen(first)
+    def third(self):
+        print(" !! ")
+
+    @listen(and_(second, third))
+    def final(self):
+        print(":)")
+
+flow = MyFirstFlow()
+
+#flow.plot() # 플로우 다이어그램 그리기 -> html 파일로 저장됨
+flow.kickoff()
