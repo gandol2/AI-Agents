d22294658d9ec81b79cf982f25203cdf371e1848
gandol2 <sskim2333@gmail.com>
2025-09-30 01:06:23 +0900

    score check

--- 03-content-pipline-agent/main.py
+++ 03-content-pipline-agent/main.py
@@ -9,6 +9,12 @@ class ContentPiplineFlowState(BaseModel):
 
     # Internal
     max_length: int = 0
+    score: int = 0
+
+    #Content
+    blog_post: str = ""
+    tweet_post: str = ""
+    linkedin_post: str = ""
 
 class ContentPiplineFlow(Flow[ContentPiplineFlowState]):
 
@@ -33,7 +39,7 @@ class ContentPiplineFlow(Flow[ContentPiplineFlowState]):
         return True
 
     @router(conduct_research)
-    def router(self):
+    def conduct_research_router(self):
         content_type = self.state.content_type
 
         if content_type == "tweet":
@@ -41,31 +47,53 @@ class ContentPiplineFlow(Flow[ContentPiplineFlowState]):
         elif content_type == "blog":
             return "make_blog"
         elif content_type == "linkedin":
-            return "make_linkedin_post"
+            return "make_linkedin"
         else:
             raise ValueError(f"알수 없는 콘텐츠 타입 입니다 : {content_type}")
 
-    @listen("make_blog")
+    @listen(or_("make_blog", "remake_blog"))
     def handle_make_blog(self):
+        # 블로그 포스트가 생성된적 있다면, 이전 블로그 포스트를 AI에게 제공하고 더 좋게 수정하도록 합니다.
+        # 그렇지 않다면 생성 합니다.
         print("블로그 콘텐츠를 만드는중 입니다...")
     
-    @listen("make_tweet")
+    @listen(or_("make_tweet", "remake_tweet"))
     def handle_make_tweet(self):
+        # 트윗 콘텐츠가 생성된적 있다면, 이전 트윗 콘텐츠를 AI에게 제공하고 더 좋게 수정하도록 합니다.
+        # 그렇지 않다면 생성 합니다.
         print("트윗 콘텐츠를 만드는중 입니다...")
     
-    @listen("make_linkedin_post")
-    def handle_make_linkedin_post(self):
+    @listen(or_("make_linkedin", "remake_linkedin"))
+    def handle_make_linkedin(self):
+        # 링크드인 콘텐츠가 생성된적 있다면, 이전 링크드인 콘텐츠를 AI에게 제공하고 더 좋게 수정하도록 합니다.
+        # 그렇지 않다면 생성 합니다.
         print("링크드인 콘텐츠를 만드는중 입니다...")
 
     @listen(handle_make_blog)
     def check_seo(self):
         print("[블로그] SEO 체크중 입니다...")
 
-    @listen(or_(handle_make_tweet, handle_make_linkedin_post))
+    @listen(or_(handle_make_tweet, handle_make_linkedin))
     def check_virality(self):
         print("[트윗, 링크드인] 화제성 체크중 입니다...")
 
-    @listen(or_(check_seo, check_virality))
+    @router(or_(check_seo, check_virality))
+    def score_router(self):
+        content_type = self.state.content_type
+        score = self.state.score
+
+        if score >= 8:
+            return "check_passed"
+        else:
+            if content_type == "blog":
+                return "remake_blog"
+            elif content_type == "linkedin":
+                return "remake_linkedin"
+            else:
+                return "remake_tweet"
+
+
+    @listen("check_passed")
     def finalize_content(self):
         print("콘텐츠를 완성하는중 입니다...")
 
