877c5d1a3e7537bba383d146a9d7c618ec4dc2fc
gandol2 <sskim2333@gmail.com>
2025-10-02 19:01:20 +0900

    temp

--- 07-financial-analyst/financial_advisor/agent.py
+++ 07-financial-analyst/financial_advisor/agent.py
@@ -1,26 +1,39 @@
 from google.adk.agents import Agent
+from google.adk.tools.agent_tool import AgentTool
 from google.adk.models.lite_llm import LiteLlm
 
 MODEL = LiteLlm("openai/gpt-4o")
 
-def get_weather(city:str):
+
+def get_weather(city: str):
     return f"The weather in {city} is sunny."
 
-def convert_units(degrees:int):
+
+def convert_units(degrees: int):
     return f"That is 40 Farenheit"
+
 
 geo_agent = Agent(
     name="GeoAgent",
-    instruction = "You are a geo agent that can answer questions about the weather.",
-    model = MODEL,
+    instruction="You are a geo agent that can answer questions about the weather.",
+    model=MODEL,
     description="Transfer to this agent when you have a geo related question.",
 )
 weather_agent = Agent(
-    name = "WeatherAgent",
-    instruction = "You are a weather agent that can answer questions about the weather.",
-    model = MODEL,
-    tools = [get_weather,convert_units],
-    sub_agents = [geo_agent]
+    name="WeatherAgent",
+    instruction="You are a weather agent that can answer questions about the weather.",
+    model=MODEL,
+    tools=[
+        get_weather,
+        convert_units
+
+
+
+
+
+    ],
+
+    sub_agents=[geo_agent]
 )
 
 root_agent = weather_agent
